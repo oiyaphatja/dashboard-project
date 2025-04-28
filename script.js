6ca6ccb0a432674c0419dcb284a7e2d05a649247
oiyaphatja <s6411423213@pkru.ac.th>
2025-04-28 11:00:51 +0700

    Delete script.js
    delete

--- script.js
+++
@@ -1,9 +0,0 @@
-// เพิ่มคลาส active เมื่อคลิกเมนู
-const links = document.querySelectorAll('nav ul li a');
-
-links.forEach(link => {
-  link.addEventListener('click', function() {
-    links.forEach(l => l.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
